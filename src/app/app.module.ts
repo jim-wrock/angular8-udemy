3f4e45c47b8ee30bbd0abe170c86dd6c4d055534
Jim Decocq <jim.decocq@gmail.com>
2019-05-29 17:12:43 +0200

    Revert "'server' components nested in 'servers' component - section 2 -18"
    This reverts commit 287c162efa156795fd2cdfe66bb029c94f2f069b.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
-import { ServerComponent } from './server/server.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
