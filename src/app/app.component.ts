b343a325499c18b3e2ad92deb2b03b9055572d94
Jim Decocq <jim.decocq@gmail.com>
2019-05-30 21:54:39 +0200

    added selector and style comments for different ways to do

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
+
   selector: 'app-root',
   templateUrl: './app.component.html',
+
+  // 2 façons d'utiliser les styles (l'une ou l'autre) ->
+
+  // fichier externe ->
+
   styleUrls: ['./app.component.css']
+
+// inline via array + backticks ->
+
+//   styles: [`
+//   h3 {
+//     color: dodgerblue
+//   }
+//   `]
+
 })
 export class AppComponent {
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+
+// SELECTORS -> 3 ways
+
   selector: 'app-servers',
+
+  // <app-servers> </app-servers>
+    // in app.component.html
+
+// selector: '[app-servers]',
+
+    // <div app-servers> </div>
+    // in app.component.html
+
+// selector: '.app-servers',
+
+    // <div class="app-servers"> </div>
+    // in app.component.html
+
+
+// TEMPLATES -> 2 ways
+
+// EXTERNAL
   templateUrl: './servers.component.html',
+
+// INLINE
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
